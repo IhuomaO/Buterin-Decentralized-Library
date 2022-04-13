cc067d86b9d54c86b78350c1cacbc419c80d50c2
degencodebeast <chukwumadavid100@gmail.com>
2022-04-13 16:38:40 +0200

    added file sharing functions

--- hardhat/contracts/DecentralizedLibrary.sol
+++ hardhat/contracts/DecentralizedLibrary.sol
@@ -3,16 +3,37 @@
 pragma solidity ^0.8.0;
 
 contract DecenLibrary {
-    //Variables
-    mapping(address => string[]) public _uploadedCIDS; //Variable to track the uploaded cids of an address
+    ///Variables
+    
+    //Variable to track the uploaded cids of an address
+    mapping(address => string[]) public _uploadedCIDS;
 
-    //FUNCTION 01
+    //Variable to store the list of files shared by an address
+    mapping(address => string[]) sharedFiles;
+    
+    //variable to store the list of files shared with an address
+    mapping(address => mapping(address => string[])) recipientFiles;
+
+    //list of uploaders
+    mapping(address => bool) uploaders;
+
+    //list of recipients
+    mapping(address => bool) public recipients;
+
+    // modifier isUploader(address _from) {
+    //     require(uploaders[_from], "sender is not an uploader");
+    //     _;
+    // }
+
+    ///FUNCTION 01
     //first upload
     function _upload(string[] memory _cidsToUpload) public {
-        _uploadedCIDS[msg.sender] = _cidsToUpload;
+        _uploadedCIDS[msg.sender] = _cidsToUpload;  
+        uploaders[msg.sender] = true;
     }
 
-    //FUNCTION 02
+
+    ///FUNCTION 02
     //subsequent uploads
     function _subsequentUpload(string[] memory _newCidsToUpload) public {
         // string[] calldata _uploadedCIDSArray;
@@ -23,7 +44,7 @@ contract DecenLibrary {
         _uploadedCIDS[msg.sender] = _updatedCIDS;
     }
 
-    //FUNCTION 03
+    ///FUNCTION 03
     //view all your uploads
     function _getListOfUploadedCIDS(address _address)
         public
@@ -33,7 +54,34 @@ contract DecenLibrary {
         return _uploadedCIDS[_address];
     }
 
-    //HELPER FUNCTION 01
+    ///FUNCTION O4
+    //A function to share files between addresses
+    //_from = address of sender
+    //_to = address of recipient
+    function shareFile(address _from, address _to, string memory _fileCid) public {
+        require(msg.sender == _from, "you are not the sender");
+
+        sharedFiles[_from].push(_fileCid);
+        recipientFiles[_from][_to].push(_fileCid);
+        recipients[_to] = true;
+    }
+
+    //FUNCTION 05
+    //A function to get shared files
+    function getSharedFiles() public view returns(string[] memory) {
+        return sharedFiles[msg.sender];
+    }
+
+    //FUNCITON 06
+    //Function to get recipient files
+    //_to = address of recipient
+    function getRecipientFiles(address _to) public view returns(string[] memory) {
+        return recipientFiles[msg.sender][_to];
+    }
+
+
+
+    ///HELPER FUNCTION 01
     function _addTwoArrays(address _address, string[] memory _newCidsToUpload)
         public
         returns (string[] memory)
@@ -45,7 +93,7 @@ contract DecenLibrary {
         return _updatedCIDS; //final array is updated
     }
 
-    //HELPER FUNCTION 02
+    ///HELPER FUNCTION 02
     //return ether balance of the wallet calling the function
     function viewBalance() internal view returns (uint256) {
         return msg.sender.balance;
