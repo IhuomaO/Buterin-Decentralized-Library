acf791c56eeb962f65065199318cdefd5c2cdaec
Samuel Osinloye <psalmueloye@gmail.com>
2022-04-15 05:27:46 +0100

    added custom hooks to upload form

--- react-app/src/Utils/helpers/fileUpload.js
+++ react-app/src/Utils/helpers/fileUpload.js
@@ -5,6 +5,8 @@ import { web3, contract } from "../contract";
 const IpfsUpload = () => {
   const [buffer, setBuffer] = useState([]);
   const [urls, setUrls] = useState([]);
+  const [metadata, setMetadata] = useState([]);
+  const [metadataCID, setMetadataCID] = useState([]);
 
   const client = create("https://ipfs.infura.io:5001/api/v0");
   const convertToBuffer = (reader) => {
@@ -12,14 +14,16 @@ const IpfsUpload = () => {
     setBuffer((prev) => [...prev, buffers]);
   };
 
-  const captureFile = (event) => {
+  const captureFile = (event, values) => {
     event.stopPropagation();
     event.preventDefault();
     const { files } = event.target;
     const fileArray = [];
     Object.keys(files).map((key) => {
       if (files[key].size < 2000000) {
-        return fileArray.push(files[key]);
+        fileArray.push(files[key]);
+        values.file = [...fileArray];
+        return fileArray;
       } else {
         return alert("File too Big");
       }
@@ -30,29 +34,42 @@ const IpfsUpload = () => {
       reader.readAsArrayBuffer(file);
       reader.onloadend = () => convertToBuffer(reader);
     });
+    console.log(fileArray);
   };
 
-  const handleUpload = async (event) => {
+  const handleUpload = async (event, values, setValues, initial) => {
     event.preventDefault();
 
+    const metaData = {
+      name: values.name,
+      description: values.description,
+      visibility: values.visibility,
+    };
     const accounts = await web3.eth.getAccounts();
 
     console.log("Sending from Metamask account: " + accounts[0]);
     try {
       buffer?.map(async (buff) => {
-        const _path = await client.add(buff);
-        const url = `https://ipfs.infura.io/ipfs/${_path.path}`;
+        const { path } = await client.add(buff);
+        const url = `https://ipfs.infura.io/ipfs/${path}`;
+        metaData.cid = path;
+        setMetadata((prev) => [...prev, metaData]);
         setUrls((prev) => [...prev, url]);
       });
+      console.log("Meta Data State: ", metadata);
+      const { path } = await client.add(metadata);
+      setMetadataCID(path);
+      await contract.methods._upload(metadataCID).send({
+        from: accounts[0],
+      });
+      setValues(initial);
+      console.log("Meta Data State: ", path, metadataCID);
+      console.log("File successfully uploaded from " + accounts[0]);
     } catch (error) {
       console.log(error);
     }
-
-    await contract.methods._upload(urls).send({
-      from: accounts[0],
-    });
-    console.log("File successfully uploaded from " + accounts[0]);
   };
+
   return { captureFile, handleUpload };
 };
 
