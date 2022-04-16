12bb8045119ad7773d8955da86ff59f6160b52b6
Abiola Fasanya <hezekiahfasanya@gmail.com>
2022-04-16 15:53:23 +0000

    fifth test completed

--- hardhat/test/decentralizedLibrary-test.js
+++ hardhat/test/decentralizedLibrary-test.js
@@ -5,25 +5,6 @@ describe("Decentralized Library", function () {
   this.timeout(0);
 
   let library, DecenLibrary, fileArr;
-  fileArr = [
-    {
-      name: "Bg Nft",
-      description: "This is my NFT",
-      image: "../img/bg.png",
-    },
-
-    {
-      name: "download NFT",
-      description: "This is my NFT",
-      image: "../img/download.png",
-    },
-
-    {
-      name: "possibilities NFT",
-      description: "This is my NFT",
-      image: "../img/possibilities1.jpg",
-    },
-  ];
 
   beforeEach(async () => {
     DecenLibrary = await ethers.getContractFactory("DecentralizedLibrary");
@@ -36,25 +17,52 @@ describe("Decentralized Library", function () {
     expect(library.address.length).to.be.gt(0);
   });
 
-
   it("should upload cid", async () => {
+    let [owner] = await ethers.getSigners();
     fileArr = ["uploaded", "food", "book", "movies"];
     const upload = await library._upload(fileArr);
     upload.wait();
     console.log(`cid Uploaded from \n ${upload.from} \n  to ${upload.to}`);
-    console.log(await library._getListOfUploadedCIDS(upload.from));
+    console.log(await library._getListOfUploadedCIDS(owner.address));
     expect(await library._getListOfUploadedCIDS(upload.from)).to.deep.equal(
       fileArr
     );
   });
 
   it("should upload and update cid", async () => {
-    fileArr = ["mango", "apple", "cherry", "banana"];
+    let [owner] = await ethers.getSigners();
+    fileArr = ["music", "photo", "ebooks", "pdf"];
     const upload = await library._subsequentUpload(fileArr);
     upload.wait();
-    console.log(await library._getListOfUploadedCIDS(upload.from));
+    console.log(await library._getListOfUploadedCIDS(owner.address));
     expect(await library._getListOfUploadedCIDS(upload.from)).to.deep.equal(
       fileArr
     );
+  });
+
+  it("should Share files with an existing customer", async () => {
+    let [owner] = await ethers.getSigners();
+    fileArr = ["book", "MacBook", "windows", "Linux"];
+    const ShareFile = await library._shareWithExisting(fileArr, owner.address);
+    ShareFile.wait();
+    console.log("\n Checking test 4 \n");
+    console.log("\n File share to: " + ShareFile.from + " \n");
+    console.log(
+      "\n File shared : " +
+        (await library._getListOfUploadedCIDS(owner.address)) +
+        " \n"
+    );
+    expect(await ShareFile.from).to.deep.equal(owner.address);
+  });
+
+  it("Should check if its an uploader", async () => {
+    let [owner] = await ethers.getSigners();
+    fileArr = ["uploaded", "food", "book", "movies"];
+    const upload = await library._upload(fileArr);
+    upload.wait();
+    let uploader = await library.isAnUploader(owner.address);
+    // uploader.wait();
+    console.log(await uploader);
+    expect(await uploader).to.deep.equal(true);
   });
 });
