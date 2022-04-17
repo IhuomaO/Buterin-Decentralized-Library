ef37f74e871c834a614c3ee75486c601f94320cf
Kainy <iartkhay@gmail.com>
2022-04-18 00:48:25 +0100

    final commit

--- react-app/src/Components/Card.js
+++ react-app/src/Components/Card.js
@@ -3,13 +3,13 @@ import React from "react";
 const Card = ({ metacid, ...props }) => {
   console.log(metacid);
   return (
-    <div className=" max-w-[400px] font-inconsolat px-10 py-5 overeflow-wrap min-h-[200px] border shadow-md rounded-md flex flex-col justify-evenly text-white bg-red-700 items-center break-words " {...props}>
-      <p className="w-full break-words font-bold">Name: <span className="font-semibold" >{metacid?.name}</span> </p>
-      <p className="w-full break-words font-bold">Address of Owner: <span className="font-semibold" >{metacid?.walletAddress}</span> </p>
-      <p className="w-full break-words font-bold">Description: <span className="font-normal" >{metacid?.description}</span> </p>
+    <div className=" max-w-[400px] font-inconsolat font-semibold text-gray-400 px-10 py-5 overeflow-wrap min-h-[200px] border shadow-md rounded-md flex flex-col justify-evenly  bg-red-700 items-center break-words " {...props}>
+      <p className="w-full break-words ">Name: <span className="font-bold text-white" >{metacid?.name}</span> </p>
+      <p className="w-full break-words">Address of Owner: <span className="font-bold text-white" >{metacid?.walletAddress}</span> </p>
+      <p className="w-full break-words ">Description: <span className="font-normal text-white" >{metacid?.description}</span> </p>
       {
         metacid?.status.toLowerCase() === 'public' &&
-        <p className="w-full break-words font-bold">Hash 2: <a href={metacid?.ipfsCID} className="underline text-xs text-gray-400 cursor-pointer" >{metacid?.ipfsHash}</a> </p>
+        <p className="w-full break-words font-bold">Hash: <a href={metacid?.ipfsCID} className="underline text-xs text-blue-100 font-normal cursor-pointer" >{metacid?.ipfsHash}</a> </p>
       }
 
     </div>
